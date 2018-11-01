4848bc77029f9f48e1159c7269e5fd68ffaebbd1
Joel Watson <joeljameswatson@gmail.com>
2018-11-01 10:38:18 -0700

    inititial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
@@ -9,7 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload yo.
           </p>
           <a
             className="App-link"
